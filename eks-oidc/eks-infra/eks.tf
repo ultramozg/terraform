f14c039ee36bb5cb61d1d6ac8a10daeb0659b548
Dmitri <d.candu@hotmail.com>
2022-04-12 12:09:28 +0300

    update the region

--- eks-oidc/eks-infra/eks.tf
+++ eks-oidc/eks-infra/eks.tf
@@ -1,7 +1,7 @@
 locals {
   name            = "ex-${replace(basename(path.cwd), "_", "-")}"
   cluster_version = "1.21"
-  region          = "us-east-1"
+  region          = "eu-west-1"
 
   tags = {
     Example    = local.name
