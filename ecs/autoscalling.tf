5f5f3fa72ca96fbb4f97ed587279c15a1f316c4a
Dmitri <d.candu@hotmail.com>
2022-05-05 14:11:05 +0300

    fix issue with nat gateway

--- ecs/autoscalling.tf
+++ ecs/autoscalling.tf
@@ -11,17 +11,26 @@ data "aws_ami" "amazon_linux_ecs" {
   }
 }
 
+/*
+resource "aws_key_pair" "mykeypair" {
+  key_name   = "mykeypair"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+*/
+
 resource "aws_launch_configuration" "ecs_launch_config" {
     image_id             = data.aws_ami.amazon_linux_ecs.id
     iam_instance_profile = aws_iam_instance_profile.ecs_agent.name
     security_groups      = [aws_security_group.ecs_sg.id]
+    //associate_public_ip_address = true
+    //key_name             = aws_key_pair.mykeypair.key_name
     instance_type        = "t3.small"
     user_data            = file("scripts/init.sh")
 }
 
 resource "aws_autoscaling_group" "failure_analysis_ecs_asg" {
     name                      = "asg"
-    vpc_zone_identifier       = [aws_subnet.pub_subnet.id]
+    vpc_zone_identifier       = [aws_subnet.priv_subnet.id]
     launch_configuration      = aws_launch_configuration.ecs_launch_config.name
 
     desired_capacity          = 1
@@ -29,4 +38,9 @@ resource "aws_autoscaling_group" "failure_analysis_ecs_asg" {
     max_size                  = 2
     health_check_grace_period = 300
     health_check_type         = "EC2"
+
+    depends_on = [
+      aws_route_table_association.priv_route,
+      aws_route_table_association.pub_route
+    ]
 }
--- ecs/network.tf
+++ ecs/network.tf
@@ -1,5 +1,31 @@
-resource "aws_internet_gateway" "internet_gateway" {
+resource "aws_vpc" "vpc" {
+    cidr_block = "10.0.0.0/16"
+    enable_dns_support   = true
+    enable_dns_hostnames = true
+    tags       = {
+        Name = "Terraform VPC"
+    }
+}
+
+resource "aws_internet_gateway" "gw" {
     vpc_id = aws_vpc.vpc.id
+}
+
+resource "aws_eip" "nat_gateway" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "nat_gw" {
+  allocation_id = aws_eip.nat_gateway.id
+  subnet_id     = aws_subnet.pub_subnet.id
+
+  tags = {
+    Name = "gw NAT"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.gw]
 }
 
 resource "aws_subnet" "pub_subnet" {
@@ -7,16 +33,35 @@ resource "aws_subnet" "pub_subnet" {
     cidr_block              = "10.0.0.0/24"
 }
 
+resource "aws_subnet" "priv_subnet" {
+    vpc_id                  = aws_vpc.vpc.id
+    cidr_block              = "10.0.1.0/24"
+}
+
 resource "aws_route_table" "public" {
     vpc_id = aws_vpc.vpc.id
 
     route {
         cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.internet_gateway.id
+        gateway_id = aws_internet_gateway.gw.id
     }
 }
 
-resource "aws_route_table_association" "route_table_association" {
+resource "aws_route_table" "private" {
+    vpc_id = aws_vpc.vpc.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        nat_gateway_id = aws_nat_gateway.nat_gw.id
+    }
+}
+
+resource "aws_route_table_association" "priv_route" {
+    subnet_id      = aws_subnet.priv_subnet.id
+    route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "pub_route" {
     subnet_id      = aws_subnet.pub_subnet.id
     route_table_id = aws_route_table.public.id
 }
