11c4bdfcd40c43bc1513802427a2da2de9e82205
Dmitri <d.candu@hotmail.com>
2022-05-06 09:48:56 +0300

    remove unused sg

--- ecs/sg.tf
+++ ecs/sg.tf
@@ -22,22 +22,3 @@ resource "aws_security_group" "ecs_sg" {
         cidr_blocks     = ["0.0.0.0/0"]
     }
 }
-
-resource "aws_security_group" "rds_sg" {
-    vpc_id      = aws_vpc.vpc.id
-
-    ingress {
-        protocol        = "tcp"
-        from_port       = 3306
-        to_port         = 3306
-        cidr_blocks     = ["0.0.0.0/0"]
-        security_groups = [aws_security_group.ecs_sg.id]
-    }
-
-    egress {
-        from_port       = 0
-        to_port         = 65535
-        protocol        = "tcp"
-        cidr_blocks     = ["0.0.0.0/0"]
-    }
-}
